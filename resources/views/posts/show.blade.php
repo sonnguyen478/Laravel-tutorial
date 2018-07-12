5af9fc2555bb4483db79df73f641e57b4cc8d720
Son Nguyen <sonnguyen478@gmail.com>
2018-07-13 01:37:13 +0900

    DB Fetch data and paginate

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,7 +1,11 @@
 @extends('layouts.app')
 
 @section('content')
+    <a href="/posts" class = "btn btn-default">Go back</a>
     <h1>{{$post->title}}</h1>
-
-
+    <div>
+        {{$post->contain}}
+    </div>
+    <hr>
+    <small>Written on {{$post->created_ate}}</small>
 @endsection
