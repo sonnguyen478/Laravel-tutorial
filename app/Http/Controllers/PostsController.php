92b99c26e3f6add7ba1b537b8978d9426e499e3a
sonnguyen478 <sonnguyen478@gmail.com>
2018-07-17 16:52:23 +0900

    Access Control

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -9,6 +9,16 @@ use App\Post;
 class PostsController extends Controller
 {
     /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth',['except'=>['index','show']]);
+    }
+
+    /**
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
@@ -88,6 +98,12 @@ class PostsController extends Controller
     public function edit($id)
     {
         $post = Post::find($id);
+
+        // Check for correct user
+        if (auth()->user()->id !== $post->user_id)
+        {
+            return redirect('/posts')->with('error','Unauthorized Page');
+        }
         return view('posts.edit')->with('post',$post);
     }
 
@@ -123,6 +139,13 @@ class PostsController extends Controller
     public function destroy($id)
     {
         $post = Post::find($id);
+
+        // Check for correct user
+        if (auth()->user()->id !== $post->user_id)
+        {
+            return redirect('/posts')->with('error','Unauthorized Page');
+        }
+                
         $post->delete();
         return redirect('/posts')->with('success', 'Post Removed');
     }
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -9,10 +9,14 @@
     <hr>
     <small>Written on {{$post->created_at}} by {{$post->user->name}}</small>
     <hr>
-    <a href="/posts/{{$post->id}}/edit" class ="btn btn-secondary my-2">Edit</a>
+    @if (!Auth::guest())
+        @if(Auth::user()->id == $post->user_id)
+        <a href="/posts/{{$post->id}}/edit" class ="btn btn-secondary my-2">Edit</a>
 
-    {!!Form::open(['action'=>['PostsController@destroy',$post->id],'method'=>'POST','class'=>'pull-right'])!!}
-        {{Form::hidden('_method', 'DELETE')}}
-        {{Form::submit('Delete',['class'=>'btn btn-danger'])}}
-    {!!Form::close()!!}
+        {!!Form::open(['action'=>['PostsController@destroy',$post->id],'method'=>'POST','class'=>'pull-right'])!!}
+            {{Form::hidden('_method', 'DELETE')}}
+            {{Form::submit('Delete',['class'=>'btn btn-danger'])}}
+        {!!Form::close()!!}
+        @endif
+    @endif
 @endsection
